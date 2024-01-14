1d4aeb4c377ae2d584c364ba0244da8ce7100e44
codfyspace <156607118+codfyspace@users.noreply.github.com>
2024-01-14 21:56:43 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { Hercai } = require('hercai');
+const app = express();
+const port = 5000;
+
+const herc = new Hercai();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post('/api/hercai', async (req, res) => {
+  const { question } = req.body;
+  const response = await herc.question({ model: "v3", content: question });
+  res.json({ reply: response.reply });
+});
+
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda çalışıyor`);
+});
